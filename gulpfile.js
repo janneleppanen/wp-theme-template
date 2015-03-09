4bf13d7fdb24b19fc50f98bef0444dbe74e61b56
Janne Leppänen <janne@janneleppanen.com>
2015-03-09 13:37:16 +0200

    Added gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
 
 gulp.task('scripts', function() { 
 	return gulp.src('js/**/*.js')
@@ -14,11 +16,17 @@ gulp.task('styles', function() {
 });
 
 gulp.task('imagemin', function() { 
-
+    return gulp.src('images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('images/'));
 });
 
 gulp.task('browsersync', function() {
 	
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'imagemin']);
