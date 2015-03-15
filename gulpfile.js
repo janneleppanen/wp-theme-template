3a10459f2aad4127527449aad575806f85140e3e
Janne Leppänen <janne@janneleppanen.com>
2015-03-15 15:35:22 +0200

    Added less compiling to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
+	less = require('gulp-less'),
+	minifyCSS = require('gulp-minify-css'),
 	imagemin = require('gulp-imagemin'),
 	pngquant = require('imagemin-pngquant');
+
+var path = {
+	themeStyles: [
+		'styles/main.less',
+		'styles/shop.less'	
+	],
+	loginStyles: [
+		'styles/login.less'
+	],
+	editorStyles: [
+		'styles/editor.less'
+	]
+};
 
 gulp.task('scripts', function() { 
 	return gulp.src('js/**/*.js')
@@ -11,8 +26,28 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('js/'));
 });
 
-gulp.task('styles', function() {
-	
+gulp.task('themeStyles', function() {
+	gulp.src(path.themeStyles)
+		.pipe(concat('main.less'))
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('styles/'));
+});
+
+gulp.task('editorStyles', function() {
+	gulp.src(path.editorStyles)
+		.pipe(concat('editor.less'))
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('styles/'));
+});
+
+gulp.task('loginStyles', function() {
+	gulp.src(path.loginStyles)
+		.pipe(concat('login.less'))
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('styles/'));
 });
 
 gulp.task('imagemin', function() { 
@@ -29,4 +64,6 @@ gulp.task('browsersync', function() {
 	
 });
 
+
+gulp.task('styles', ['themeStyles', 'loginStyles', 'editorStyles']);
 gulp.task('default', ['scripts', 'imagemin']);
