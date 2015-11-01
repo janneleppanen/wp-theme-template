292c1c4a76ef57213e25bef752afb70da22b4cab
Janne Leppänen <janne@janneleppanen.com>
2015-11-01 08:48:43 +0200

    Separate header files

---
+++ header-top.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js" prefix="og: http://ogp.me/ns#">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="HandheldFriendly" content="True">
+    <link rel="shortcut icon" type="image/ico" href="<?php echo THEME_DIR; ?>/favicon.ico">
+    <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width">
+
+    <?php wp_head(); ?>
+</head>
--- header.php
+++ header.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js" prefix="og: http://ogp.me/ns#">
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="HandheldFriendly" content="True">
-<link rel="shortcut icon" type="image/ico" href="<?php echo THEME_DIR; ?>/favicon.ico">
-<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width">
+<?php get_template_part('header', 'top'); ?>
 
-<?php wp_head(); ?>
-</head>
 <body <?php body_class(); ?>>
 
 <div id="top" class="site-wrapper">
