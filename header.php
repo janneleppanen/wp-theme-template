df2a0847ac0e07d4235d90599ca8d91addd3a29d
Janne Leppänen <janne@janneleppanen.com>
2016-04-21 19:56:52 +0300

    Logo wrapped by h1 only at frontpage they say. SEO thing.

--- header.php
+++ header.php
@@ -6,11 +6,19 @@
 
     <header class="site-header" role="banner">
         <div class="header-content">
+            <?php if ( is_front_page() ) : ?>
             <h1 class="site-title">
                 <a href="<?php echo esc_url( home_url('/') ); ?>" rel="home">
                     <?php bloginfo( 'name' ); ?>
                 </a>
             </h1>
+            <?php else : ?>
+            <p class="site-title">
+                <a href="<?php echo esc_url( home_url('/') ); ?>" rel="home">
+                    <?php bloginfo( 'name' ); ?>
+                </a>
+            </p>
+            <?php endif; ?>
 
             <nav class="main-menu" role="navigation">
                 <a class="sr-only skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
